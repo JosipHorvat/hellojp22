dc21aa3071cdfd7ba20e1b3d8d63b81f10f10d8d
Josip Horvat <josiph988@gmail.com>
2020-06-03 19:10:41 +0100

    Konacno rjesenje XD

--- Zadatak01.java
+++ Zadatak01.java
@@ -9,19 +9,21 @@ public class Zadatak01 {
 		int broj1 = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
 		int broj2 = Integer.parseInt(JOptionPane.showInputDialog("unesi drugi broj"));
 		int broj3 = Integer.parseInt(JOptionPane.showInputDialog("unesi treci broj"));
-		if (broj1> broj2 && broj1 > broj3) {
+		
+		if (broj1>= broj2 && broj1 >= broj3) {
 			System.out.println(broj1);
 		}
-		else if (broj2 > broj1 && broj2 > broj3){
+		else if (broj2 >= broj1 && broj2 >= broj3){
 			System.out.println(broj2);
 			
 		}
-		else if (broj3 > broj2 && broj3 >broj1) {
+		else if (broj3 >= broj2 && broj3 >=broj1) {
 			System.out.println(broj3);
 		}
 		else if (broj1 == broj2 && broj1 == broj3 && broj2 == broj1 && broj2== broj3 && broj3 == broj2 && broj3 == broj1) {
 			System.out.println(broj1);
 		}
+		
 			
 		
 	}
